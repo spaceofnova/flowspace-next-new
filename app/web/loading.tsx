320c440006a1368aafee627a3a3911086d6b6f3c
spaceofnova <mcrealms23@gmail.com>
2024-04-21 20:26:59 -0500

    Added basic loader to all pages after /web

---
+++ app/web/loading.tsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+  // You can add any UI inside Loading, including a Skeleton.
+  return (
+    <div className="w-full h-full flex flex-col justify-center items-center">
+      <span className="loading loading-ring loading-lg"></span>
+    </div>
+  );
+}
